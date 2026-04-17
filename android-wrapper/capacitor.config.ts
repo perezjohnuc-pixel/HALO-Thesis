d522be6840681fc6da4576b40e9f8165e103d646
Andre <perezjohn.uc@gmail.com>
2026-04-16 23:06:19 -0400

    remove the server configuration from the capacitor.config.ts file, as it is not needed for the Android wrapper. This will prevent any potential issues with the development server when building the Android app.

--- android-wrapper/capacitor.config.ts
+++ android-wrapper/capacitor.config.ts
@@ -5,9 +5,7 @@ const config: CapacitorConfig = {
   appName: "HALO Thesis",
   webDir: "../web/dist",
   bundledWebRuntime: false,
-  server: { url: "https://halo-thesis-adrqsfcf3-john-andres-projects-d817ccb7.vercel.app",
 
-}
 };
 
 export default config;
