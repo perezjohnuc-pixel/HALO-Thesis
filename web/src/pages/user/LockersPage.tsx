d7c6614ebf4d4a9b31edc93eadf3f495e049157c
perezjohnuc-pixel <perezjohn.uc@gmail.com>
2026-05-20 12:54:17 -0400

    Add unattendedChargeStartsAt to locker state
    Added unattendedChargeStartsAt property to locker state.

--- web/src/pages/user/LockersPage.tsx
+++ web/src/pages/user/LockersPage.tsx
@@ -193,6 +193,7 @@ export default function LockersPage() {
         cancelledAt: null,
         expiredAt: null,
         pickupReadyAt: null,
+        unattendedChargeStartsAt: null,
       });
 
       batch.update(lockerRef, {
